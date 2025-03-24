ceeaa557dc55dccc9dc48f5c2cfdae65d019ad76
rupomrubaiyat <rupomsaad@gmail.com>
2025-03-24 13:14:56 +0600

    fizz buzz problem solved

--- practice_C_language/Temporary/temporary.c
+++ practice_C_language/Temporary/temporary.c
@@ -34,7 +34,7 @@ int main()
     return 0;
 }*/ 
 
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
     float a, b;
@@ -57,4 +57,42 @@ int main() {
     }
 
     return 0;
+}*/
+
+
+#include<stdio.h>
+int main(){
+    int n ,i ;
+
+
+    printf("enter an integer");
+    scanf("%d",&n);
+    for ( i = 1; i<=n; i++)
+    {
+
+        if (i%5==0 && i%3==0)
+        {
+            printf("Fizzbuzz\n");
+            
+        }
+
+        else if (i%3==0)
+        {
+            printf("Fizz\n");
+        }
+        else if (i%5==0)
+        {
+           printf("Buzz\n");
+        }        
+        else
+            printf("%d \n",i);
+        
+
+
+        
+        
+    
+    }
+    
+
 }
