861565dc97a8c67f5464bf947d6514211a6640f7
rupomrubaiyat <rupomsaad@gmail.com>
2025-03-25 15:34:15 +0600

    okkk

---
+++ Problem solving/pblm2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    int a,b;
+    printf("enter 1st integer ");
+    scanf("%d",&a);
+    printf("enter 2nd integer ");
+    scanf("%d",&b);
+
+    printf("Sum=%d",a+b);
+
+
+}
